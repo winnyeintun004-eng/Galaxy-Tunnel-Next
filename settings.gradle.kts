0c2d808edeebcf8fe7f81de1e3fdc1edb83f92b4
winnyeintun004-eng <winnyeintun004@gmail.com>
2026-07-02 01:34:26 +0630

    Modify settings.gradle.kts for v2ray inclusion
    Updated project settings to include v2ray module and adjusted project name format.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,22 @@
 pluginManagement {
-  repositories {
-    google()        // ← content filter ဖြုတ်
-    mavenCentral()
-    gradlePluginPortal()
-  }
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
-
-plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
 dependencyResolutionManagement {
-  repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories {
-    google()
-    mavenCentral()
-  }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        flatDir {
+            dirs("v2ray/libs")
+        }
+    }
 }
 
-rootProject.name = "Galaxy Tunnel Next"  // ← app name နဲ့ ကိုက်အောင်
-
+rootProject.name = "Galaxy-Tunnel-Next"
 include(":app")
+include(":v2ray")  // ← ADD THIS LINE
