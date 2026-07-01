d39aa51fbc168064d434b7be310fff293d76e47c
winnyeintun004-eng <winnyeintun004@gmail.com>
2026-07-02 01:30:55 +0630

    Implement V2rayController with connection management

---
+++ v2ray/src/main/java/com/dev7dev/v2ray/V2rayController.java
@@ -0,0 +1,48 @@
+package com.dev7dev.v2ray;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class V2rayController {
+    
+    public enum ConnectionState {
+        CONNECTED, CONNECTING, DISCONNECTED
+    }
+    
+    private static String appName = "";
+    private static int iconResId = 0;
+    
+    public static void init(Context context, int iconResourceId, String applicationName) {
+        iconResId = iconResourceId;
+        appName = applicationName;
+    }
+    
+    public static String getCoreVersion() {
+        return "Xray-core v1.8.4";
+    }
+    
+    public static ConnectionState getConnectionState() {
+        return ConnectionState.DISCONNECTED;
+    }
+    
+    public static void startV2ray(Context context, String remark, String config, String packageName) {
+        Intent intent = new Intent(context, V2RayVpnService.class);
+        intent.setAction("START");
+        intent.putExtra("REMARK", remark);
+        intent.putExtra("CONFIG", config);
+        intent.putExtra("PACKAGE", packageName);
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent);
+        } else {
+            context.startService(intent);
+        }
+    }
+    
+    public static void stopV2ray(Context context) {
+        Intent intent = new Intent(context, V2RayVpnService.class);
+        intent.setAction("STOP");
+        context.startService(intent);
+    }
+}
