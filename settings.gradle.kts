ba333a965e792451f43bce3c08f5127ed4e847c6
winnyeintun004-eng <winnyeintun004@gmail.com>
2026-07-02 15:36:54 +0630

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,18 +5,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        flatDir {
-            dirs("v2ray/libs")
-        }
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "Galaxy-Tunnel-Next"
 include(":app")
-include(":v2ray")  // ← ADD THIS LINE
+include(":v2ray")
